5bb778e2a01601ebe3bd571a8fe5c8b014e47050
Dan Stoianovici <stoianovici.dan.s@gmail.com>
2020-05-15 11:59:37 -0600

    paarsed csv

--- src/owb_kinematics_test.cpp
+++ src/owb_kinematics_test.cpp
@@ -1,3 +1,10 @@
+/*
+Testing program to verify Omni Wheel Kinematics
+Loads RPY positions from CSV, measures actual RPY with IMU
+
+
+*/
+
 #include <ros/ros.h>
 #include <equivalent_manipulator_ik/eq_man_ik.h>
 #include <stdio.h>
@@ -22,69 +29,69 @@ int mt2_e;
 std::vector<float> goal_rot(3);
 
 
-// std_msgs::Int32MultiArray create_goal_msg(std::vector<int> mot_pos){
-//     std_msgs::Int32MultiArray goal_msg;
-//     goal_msg.data.resize(3);
-//     mot_pos.resize(3);
+std_msgs::Int32MultiArray create_goal_msg(std::vector<int> mot_pos){
+    std_msgs::Int32MultiArray goal_msg;
+    goal_msg.data.resize(3);
+    mot_pos.resize(3);
 
-//     goal_msg.data[0] = mot_pos[0];
-//     goal_msg.data[1] = mot_pos[1];
-//     goal_msg.data[2] = mot_pos[2];
+    goal_msg.data[0] = mot_pos[0];
+    goal_msg.data[1] = mot_pos[1];
+    goal_msg.data[2] = mot_pos[2];
 
-//     return goal_msg;
-// }
-
-
-// void error_callback(const std_msgs::Int32MultiArray& error_msg){
-
-//     mt0_e = error_msg.data[0];
-//     mt1_e = error_msg.data[1];
-//     mt2_e = error_msg.data[2];
-
-//     if(mt0_e==0 && mt1_e == 0 && mt2_e == 0){
-//         settle_flag = 1;
-//     }
-
-//     ROS_INFO("OW Error: (%d, %d, %d)", error_msg.data[0], error_msg.data[1], error_msg.data[2]);
-// }
+    return goal_msg;
+}
 
 
-// void imu_callback(const sensor_msgs::Imu& imu_msg){
-//     tf2::Quaternion quat_tf;
-//     tf2::convert(imu_msg.orientation,quat_tf);
+void error_callback(const std_msgs::Int32MultiArray& error_msg){
 
-//     tf2Scalar roll, pitch, yaw;
+    mt0_e = error_msg.data[0];
+    mt1_e = error_msg.data[1];
+    mt2_e = error_msg.data[2];
 
-//     tf2::Matrix3x3(quat_tf).getRPY(roll,pitch,yaw);
+    if(mt0_e==0 && mt1_e == 0 && mt2_e == 0){
+        settle_flag = 1;
+    }
 
-//     goal_rot[0] = roll;
-//     goal_rot[1] = pitch;
-//     goal_rot[2] = yaw;
-
-//     //ROS_INFO("RPY: %f ,%f ,%f", roll,pitch,yaw);
-// }
+    ROS_INFO("OW Error: (%d, %d, %d)", error_msg.data[0], error_msg.data[1], error_msg.data[2]);
+}
 
 
-// void torsoRPY_callback(const geometry_msgs::Pose torso_quat){
-//     tf2Scalar roll, pitch, yaw;
-//     tf2::Matrix3x3 torso_rot;
-//     tf2::Quaternion quat_temp;
+void imu_callback(const sensor_msgs::Imu& imu_msg){
+    tf2::Quaternion quat_tf;
+    tf2::convert(imu_msg.orientation,quat_tf);
 
-//     quat_temp.setW(torso_quat.orientation.w);
-//     quat_temp.setX(torso_quat.orientation.x);
-//     quat_temp.setY(torso_quat.orientation.y);
-//     quat_temp.setZ(torso_quat.orientation.z);
+    tf2Scalar roll, pitch, yaw;
 
-//     torso_rot.setRotation(quat_temp);
+    tf2::Matrix3x3(quat_tf).getRPY(roll,pitch,yaw);
 
-//     torso_rot.getRPY(roll,pitch,yaw);
+    goal_rot[0] = roll;
+    goal_rot[1] = pitch;
+    goal_rot[2] = yaw;
 
-//     std::cout << roll << "," << pitch << "," << yaw << std::endl;
+    //ROS_INFO("RPY: %f ,%f ,%f", roll,pitch,yaw);
+}
 
-//     goal_rot[0] = roll;
-//     goal_rot[1] = pitch;
-//     goal_rot[2] = yaw;
-// }
+
+void torsoRPY_callback(const geometry_msgs::Pose torso_quat){
+    tf2Scalar roll, pitch, yaw;
+    tf2::Matrix3x3 torso_rot;
+    tf2::Quaternion quat_temp;
+
+    quat_temp.setW(torso_quat.orientation.w);
+    quat_temp.setX(torso_quat.orientation.x);
+    quat_temp.setY(torso_quat.orientation.y);
+    quat_temp.setZ(torso_quat.orientation.z);
+
+    torso_rot.setRotation(quat_temp);
+
+    torso_rot.getRPY(roll,pitch,yaw);
+
+    std::cout << roll << "," << pitch << "," << yaw << std::endl;
+
+    goal_rot[0] = roll;
+    goal_rot[1] = pitch;
+    goal_rot[2] = yaw;
+}
 
 
 
@@ -98,41 +105,37 @@ std::vector<int> ik_to_motors(float gear_ration, std::vector<float> ik){
     return mot_out;
 }
 
-float deg_2_rad(int deg){
+float deg_2_rad(float deg){
     return deg*(PI_/180);
 }
 
 
-std::vector<std::vector<float>> read_test_data(const char* file_name){
+std::vector<std::vector<float>> read_test_data(std::string file_name){
     std::ifstream file;
-    std::vector<float> goal_rpy(3);
     std::vector<std::vector<float>> goal_rpy_list;
-    uint i = 0;
-
     std::string line;
 
-    //file.open("/home/Stoianovici/catkin_ws/src/nyku_omniwheel_base/testing_data/goal_pos_test_roll.csv", std::fstream::in);
+    file.open(file_name.c_str(), std::ios::in);
 
-    if(file.is_open() == 1){
+    if(file.is_open()){
         while(std::getline(file, line)){
-            std::cout << i << std::endl;
             std::stringstream ss(line);
             std::string rad_s;
-            uint j  = 0;
-            while(std::getline(ss,rad_s,',')){
-       
-                float rad_f = std::atof(rad_s.c_str());
-                std::cout<<rad_f<<std::endl;
-                goal_rpy[j] = rad_f;
-                j++;
-            };
 
+            std::vector<float> goal_rpy(3);
+            float rad_f;
+
+            uint i = 0;
+            while(std::getline(ss,rad_s,',')){
+                rad_f = deg_2_rad(std::atof(rad_s.c_str()));
+                goal_rpy[i] = rad_f;
+                i++;
+            };
             goal_rpy_list.push_back(goal_rpy);
-            i++;
         }
     }
 
-    else std::cout << "Could not open poot file"  << std::endl;
+    else std::cout << "Could not open file"  << std::endl;
 
     //goal_rpy_list.resize(i);
 
@@ -146,15 +149,19 @@ void print_file(std::vector<std::vector<float>> file_list){
 }
 
 int main(int argc, char* argv[]){
+    ros::init(argc, argv, "OWB_IK_node");
+    ros::NodeHandle nh;
+    ros::Publisher goal_pub = nh.advertise<std_msgs::Int32MultiArray>("OWB_goal_pos", 1000);
+    ros::Subscriber pos_error_sub = nh.subscribe("OWB_pos_error", 500, &error_callback);
+    ros::Subscriber imu_sub = nh.subscribe("imu", 500, &imu_callback);
+    ros::Subscriber cam_sub = nh.subscribe("torso_rotation", 500, &torsoRPY_callback);
+    ros::Rate e_rate(100);
 
+    std::string file_name = "goal_pos_test_roll.csv";
+    std::vector<std::vector<float>> test_data;
 
-    // ros::init(argc, argv, "OWB_IK_node");
-    // ros::NodeHandle nh;
-    // ros::Publisher goal_pub = nh.advertise<std_msgs::Int32MultiArray>("OWB_goal_pos", 1000);
-    // ros::Subscriber pos_error_sub = nh.subscribe("OWB_pos_error", 500, &error_callback);
-    // //ros::Subscriber imu_sub = nh.subscribe("imu", 500, &imu_callback);
-    // ros::Subscriber cam_sub = nh.subscribe("torso_rotation", 500, &torsoRPY_callback);
-    // ros::Rate e_rate(100);
+    test_data = read_test_data(file_name);
+    print_file(test_data);
 
     // std_msgs::Int32MultiArray goal_pos;
     // goal_pos.data.resize(3);
