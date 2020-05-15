7341b0926474de5571d421578462ccdbccf7bb37
Dan Stoianovici <stoianovici.dan.s@gmail.com>
2020-05-14 18:04:41 -0600

    testing program

--- src/owb_kinematics_test.cpp
+++ src/owb_kinematics_test.cpp
@@ -20,31 +20,31 @@ int mt2_e;
 std::vector<float> goal_rot(3);
 
 
-std_msgs::Int32MultiArray create_goal_msg(std::vector<int> mot_pos){
-    std_msgs::Int32MultiArray goal_msg;
-    goal_msg.data.resize(3);
-    mot_pos.resize(3);
+// std_msgs::Int32MultiArray create_goal_msg(std::vector<int> mot_pos){
+//     std_msgs::Int32MultiArray goal_msg;
+//     goal_msg.data.resize(3);
+//     mot_pos.resize(3);
 
-    goal_msg.data[0] = mot_pos[0];
-    goal_msg.data[1] = mot_pos[1];
-    goal_msg.data[2] = mot_pos[2];
+//     goal_msg.data[0] = mot_pos[0];
+//     goal_msg.data[1] = mot_pos[1];
+//     goal_msg.data[2] = mot_pos[2];
 
-    return goal_msg;
-}
+//     return goal_msg;
+// }
 
 
-void error_callback(const std_msgs::Int32MultiArray& error_msg){
+// void error_callback(const std_msgs::Int32MultiArray& error_msg){
 
-    mt0_e = error_msg.data[0];
-    mt1_e = error_msg.data[1];
-    mt2_e = error_msg.data[2];
+//     mt0_e = error_msg.data[0];
+//     mt1_e = error_msg.data[1];
+//     mt2_e = error_msg.data[2];
 
-    if(mt0_e==0 && mt1_e == 0 && mt2_e == 0){
-        settle_flag = 1;
-    }
+//     if(mt0_e==0 && mt1_e == 0 && mt2_e == 0){
+//         settle_flag = 1;
+//     }
 
-    ROS_INFO("OW Error: (%d, %d, %d)", error_msg.data[0], error_msg.data[1], error_msg.data[2]);
-}
+//     ROS_INFO("OW Error: (%d, %d, %d)", error_msg.data[0], error_msg.data[1], error_msg.data[2]);
+// }
 
 
 // void imu_callback(const sensor_msgs::Imu& imu_msg){
@@ -63,26 +63,26 @@ void error_callback(const std_msgs::Int32MultiArray& error_msg){
 // }
 
 
-void torsoRPY_callback(const geometry_msgs::Pose torso_quat){
-    tf2Scalar roll, pitch, yaw;
-    tf2::Matrix3x3 torso_rot;
-    tf2::Quaternion quat_temp;
+// void torsoRPY_callback(const geometry_msgs::Pose torso_quat){
+//     tf2Scalar roll, pitch, yaw;
+//     tf2::Matrix3x3 torso_rot;
+//     tf2::Quaternion quat_temp;
 
-    quat_temp.setW(torso_quat.orientation.w);
-    quat_temp.setX(torso_quat.orientation.x);
-    quat_temp.setY(torso_quat.orientation.y);
-    quat_temp.setZ(torso_quat.orientation.z);
+//     quat_temp.setW(torso_quat.orientation.w);
+//     quat_temp.setX(torso_quat.orientation.x);
+//     quat_temp.setY(torso_quat.orientation.y);
+//     quat_temp.setZ(torso_quat.orientation.z);
 
-    torso_rot.setRotation(quat_temp);
+//     torso_rot.setRotation(quat_temp);
 
-    torso_rot.getRPY(roll,pitch,yaw);
+//     torso_rot.getRPY(roll,pitch,yaw);
 
-    std::cout << roll << "," << pitch << "," << yaw << std::endl;
+//     std::cout << roll << "," << pitch << "," << yaw << std::endl;
 
-    goal_rot[0] = roll;
-    goal_rot[1] = pitch;
-    goal_rot[2] = yaw;
-}
+//     goal_rot[0] = roll;
+//     goal_rot[1] = pitch;
+//     goal_rot[2] = yaw;
+// }
 
 
 
@@ -98,6 +98,43 @@ std::vector<int> ik_to_motors(float gear_ration, std::vector<float> ik){
 
 float deg_2_rad(int deg){
     return deg*(PI_/180);
+}
+
+
+std::vector<std::vector<float>> read_test_data(const char* file_name){
+    std::ifstream file("~/catkin_ws/src/nyku_omniwheel_base/testing_data/goal_pos_test_roll.csv",std::ios::in);
+    std::vector<float> goal_rpy(3);
+    std::vector<std::vector<float>> goal_rpy_list;
+    uint i = 0;
+
+    std::string line;
+
+    //file.open("/home/Stoianovici/catkin_ws/src/nyku_omniwheel_base/testing_data/goal_pos_test_roll.csv", std::fstream::in);
+
+    if(file.is_open() == 1){
+        while(std::getline(file, line)){
+            std::cout << i << std::endl;
+            std::stringstream ss(line);
+            std::string rad_s;
+            uint j  = 0;
+            while(std::getline(ss,rad_s,',')){
+       
+                float rad_f = std::atof(rad_s.c_str());
+                std::cout<<rad_f<<std::endl;
+                goal_rpy[j] = rad_f;
+                j++;
+            };
+
+            goal_rpy_list.push_back(goal_rpy);
+            i++;
+        }
+    }
+
+    else std::cout << "Could not open poot file"  << std::endl;
+
+    //goal_rpy_list.resize(i);
+
+    return goal_rpy_list;
 }
 
 
